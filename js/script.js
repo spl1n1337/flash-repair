d4d9a69a6a178a038a98d7d47e0f73c70e0cb10c
spl1n1337 <panov1337.lp@gmail.com>
2023-03-24 16:56:32 +0400

    complete all pages

--- js/script.js
+++ js/script.js
@@ -13,5 +13,29 @@ setInterval(function() {
 
 $('.prevent').click(function (e) { 
   e.preventDefault();  
-  console.log(this);
 });
+
+// let trigers = document.querySelectorAll('.base-triger');
+
+// trigers.forEach(function(triger) {
+//   triger.addEventListener('click', function(i) {
+//     i.preventDefault();
+//     this.querySelector(".base-triger-icon").classList.toggle('active-triger');
+//     let list = this.nextElementSibling;
+//     if(this.querySelector(".base-triger-icon").classList.contains('active-triger')) {
+//       list.slideDown()
+//       // this.nextElementSibling.style.height = 'auto';
+//     }
+    
+//   });
+// });
+
+$(".base-triger").on("click", function(e) {
+  e.preventDefault();
+  $(this).find(".base-triger-icon").toggleClass("active-triger");
+  if($(this).find(".base-triger-icon").hasClass("active-triger")) {
+  $(this).next().slideDown(600);
+  }else {
+    $(this).next().slideUp(600);
+  }
+  });
